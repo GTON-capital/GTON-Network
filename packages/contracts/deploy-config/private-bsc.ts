51f5054de01a5633bfa78111be66b64217601589
Slava Startsev <slava@gton.capital>
2022-12-03 22:19:09 +0300

    chore: deploy for segregation of services

--- packages/contracts/deploy-config/private-bsc.ts
+++ packages/contracts/deploy-config/private-bsc.ts
@@ -8,13 +8,13 @@ const config = {
   ctcEnqueueGasCost: 60_000,
   sccFaultProofWindowSeconds: 604800,
   sccSequencerPublishWindowSeconds: 12592000,
-  ovmSequencerAddress: '0x2943b2328A2db2B5404d2706a2e96053bed8fD03',
-  ovmProposerAddress: '0x40574a38d7497C182805E471272EF871b9EE0869',
+  ovmSequencerAddress: '0x002E7FB7f50A3b522F39A3F2fF19b2A01Ec4F4C2',
+  ovmProposerAddress: '0xA3419c9A88121aE7846d93790E8ddC3974c1dD10',
   ovmBlockSignerAddress: '0x00000398232E2064F896018496b4b44b3D62751F',
-  ovmFeeWalletAddress: '0x8Ef87a43FF22aF94cAd93D91F5d7fc6C8C3739cC',
-  ovmAddressManagerOwner: '0x8Ef87a43FF22aF94cAd93D91F5d7fc6C8C3739cC',
-  ovmGasPriceOracleOwner: '0x8Ef87a43FF22aF94cAd93D91F5d7fc6C8C3739cC',
-  ovmWhitelistOwner: '0x8Ef87a43FF22aF94cAd93D91F5d7fc6C8C3739cC',
+  ovmFeeWalletAddress: '0xFe7B3F3dEAd43A7DfFFbC49D037050030B641999',
+  ovmAddressManagerOwner: '0xFe7B3F3dEAd43A7DfFFbC49D037050030B641999',
+  ovmGasPriceOracleOwner: '0xFe7B3F3dEAd43A7DfFFbC49D037050030B641999',
+  ovmWhitelistOwner: '0xFe7B3F3dEAd43A7DfFFbC49D037050030B641999',
 }
 
 export default config
